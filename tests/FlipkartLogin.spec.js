8d29178123f6bcf7c77b474123ba6a1376602075
Sabi Raje <sabiraje952518@gmail.com>
2025-11-27 22:20:30 +0530

    comment

--- tests/FlipkartLogin.spec.js
+++ tests/FlipkartLogin.spec.js
@@ -4,6 +4,15 @@ test("Valid Login", async ({ page }) => {
 
   await page.goto("https://www.flipkart.com/");
 
-  await page.locator("(//a[@class='_1TOQfO'])[1]").click();
+  await page.locator('//*[@id="container"]/div/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/header/div[2]/div[2]/div/div/div/div/a/span').click();
     
-  })
+  await page.locator('//*[@id="container"]/div/div[3]/div/div[2]/div/form/div[1]/input').click();
+    await page.locator('//*[@id="container"]/div/div[3]/div/div[2]/div/form/div[1]/input').fill('6207662994');
+    await page.locator("//button[@class='QqFHMw twnTnD _7Pd1Fp']").click();
+    await page.waitForTimeout(5000);
+
+
+  
+})
+
+//Sabi Raje
